e49c4f6709fb7f9952d894328722d01dc42ed195
Joel Grenon <joelgrenon@gmail.com>
2019-01-11 13:54:22 -0500

    Change license reference

--- src/main.js
+++ src/main.js
@@ -21,7 +21,7 @@ const formatOut = bFormat({ outputMode: 'short' });
 
 const rootLogger = bunyan.createLogger({ name: process.env.LOG_NAME || 'xlr8', stream: formatOut, level: process.env.LOG_LEVEL || 'debug' });
 
-rootLogger.info("XLR8 Version 1. Licensed under Apache 2.0 License.");
+rootLogger.info("XLR8 Version 1. Licensed under MIT License.");
 
 global.XLR8 = module.exports = {
     Process: require('./process'),
